7c21e7ffd1a7d2e774cd617381b4cba004938b5c
Alfred Gualberto <alfredgualberto09@gmail.com>
2025-03-15 23:02:21 +0800

    added methods

--- Program.cs
+++ Program.cs
@@ -2,23 +2,24 @@
 {
     internal class Program
     {
+        //global variables
+        static byte systemOption;
+        static string dailyReport = "No current report", updateDailyReport;
         static void Main(string[] args)
         {
-            //variables used
+            //local variables
             string userName, password;
-            bool loginLoop = false, optionLoop = false;
-            char opt;
-            string dlyRep = "No current report", updDlyRep;
-
+            bool loginLoop = false;
+            
             Console.WriteLine("Welcome to Store Tracker");
 
             //loop until login is successful
             while (!loginLoop)
             {
                 //get username and password
-                Console.WriteLine("Username: ");
+                Console.Write("Username: ");
                 userName = Console.ReadLine();
-                Console.WriteLine("Password: ");
+                Console.Write("Password: ");
                 password = Console.ReadLine();
 
                 //check if username and password are correct
@@ -26,33 +27,20 @@
                 {
                     Console.WriteLine("Login Successful!\n");
 
-                    //loop the option
-                    while (!optionLoop)
+                    //show options/functions on loop
+                    do
                     {
-                        //show options
-                        Console.WriteLine("""
-                            [1] View Inventory
-                            [2] Update Inventory
-                            [3] View Last Daily Report
-                            [4] Create Daily Report
-                            [5] View Last Earning Report
-                            [6] Create Earning Report
-                            [7] Exit
-                            """);
-                        opt =Convert.ToChar(Console.ReadLine());
+                        ShowOptions();
 
                         //function depending on choice made
-                        switch(opt)
+                        switch (systemOption)
                         {
-                            case '1':
-                                Console.WriteLine("View Inventory");
-                                //view current inventory
+                            case 1:
+                                Console.WriteLine("View Inventory"); //view current inventory
                                 break;
-                            case '2':
+                            case 2:
                                 Console.WriteLine("Update Inventory");
                                 //show options for inventory; 1 frame - scroll pane - add/remove item - textfield
-                                
-
 
                                 /*
                                 add item
@@ -60,36 +48,30 @@
                                 update quantity of each item
                                  */
                                 break;
-                            case '3':
-                                updDlyRep = dlyRep;
-                                Console.WriteLine($"Last Daily Report: {updDlyRep}");
-                                //show the last daily report
+                            case 3:
+                                DailyReport(3); //show the last daily report
                                 break;
-                            case '4':
-                                Console.WriteLine("Create Daily Report: ");
-                                dlyRep = Console.ReadLine();
-                                Console.WriteLine("Report Updated.");
-                                //create a daily report for that day
+                            case 4:
+                                DailyReport(4); //create a daily report for that day
                                 break;
-                            case '5':
+                            case 5:
                                 Console.WriteLine("View Last Earning Report");
                                 //show the last earning report
                                 break;
-                            case '6':
+                            case 6:
                                 Console.WriteLine("Create Earning Report");
                                 //1 frame - textfields for earnings, profit (money update)
                                 //create an earning report which includes earnings, lost, and profit
                                 break;
-                            case '7':
+                            case 7:
                                 //exit the program
-                                optionLoop = true;
                                 break;
                             default:
                                 Console.WriteLine("Invalid Option");
                                 break;
                         }
                         Console.WriteLine();
-                    }
+                    } while (systemOption != 7);
                     loginLoop = true;
                 }
                 else
@@ -98,5 +80,42 @@
                 }
             }                       
         }
-    }
+
+        static void ShowOptions()
+        {
+            //show options
+            Console.Write("""
+                            [1] View Inventory
+                            [2] Update Inventory
+                            [3] View Last Daily Report
+                            [4] Create Daily Report
+                            [5] View Last Earning Report
+                            [6] Create Earning Report
+                            [7] Exit
+                            UserInput: 
+                            """);
+            systemOption = Convert.ToByte(Console.ReadLine());
+        }
+
+        static void DailyReport(byte userChoice)
+        {
+            if(userChoice == 3)
+            {
+                updateDailyReport = dailyReport;
+                Console.WriteLine($"Last Daily Report: {updateDailyReport}");
+            }
+            else if (userChoice == 4)
+            {
+                Console.Write("Create Daily Report: ");
+                dailyReport = Console.ReadLine();
+                Console.WriteLine("Report Updated.");
+                Console.WriteLine($"Last Daily Report: {dailyReport}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input.");
+            }
+
+        }
+    }   
 }
