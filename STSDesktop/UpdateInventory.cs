b439a95996780d2e8d36bf36e7eadf1891825825
Alfred Gualberto <alfredgualberto09@gmail.com>
2025-06-27 23:26:43 +0800

    add item function

--- STSDesktop/UpdateInventory.cs
+++ STSDesktop/UpdateInventory.cs
@@ -20,7 +20,37 @@ namespace STSDesktop
 
         private void btnAddItem_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                string itemName = txbItemName.Text.Trim();
+                int itemQuantity = int.Parse(txbItemQuantity.Text);
+                double itemPrice = double.Parse(txbItemPrice.Text);
+                
+                if (!InventoryBL.CheckItemInInventory(itemName))
+                {
+                    if (InventoryBL.CheckItemQuantity(itemQuantity))
+                    {
+                        InventoryBL.AddItem(itemName, itemQuantity, (int)itemPrice);
+                        MessageBox.Show($"'{itemName}' added to the inventory.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        txbItemName.Text = "";
+                        txbItemQuantity.Text = "";
+                        txbItemPrice.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid item quantity.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Item already exist in the inventory.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                
+            } catch
+            {
+                MessageBox.Show("Invalid Inputs. All fields must be filled.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
         }
 
         private void btnRemoveItem_Click(object sender, EventArgs e)
