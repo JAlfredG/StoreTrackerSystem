43333d574f39f9f5ea6df491d8470895c54c217b
Alfred Gualberto <138979680+JAlfredG@users.noreply.github.com>
2025-06-27 23:52:41 +0800

    STS API

---
+++ WebApplication2/Controllers/STSAccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using STSCommon;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class STSAccountController : ControllerBase
+    {
+        static STSBusinessDataLogic.AccountBL accountService = new STSBusinessDataLogic.AccountBL();
+
+        [HttpGet]
+        public IEnumerable<StoreAccount> GetUsers()
+        {
+            var accounts = accountService.GetStoreAccounts();
+
+            return accounts;
+        }
+    }
+}
---
+++ WebApplication2/Controllers/STSInventoryController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using STSBusinessDataLogic;
+using STSCommon;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class STSInventoryController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IEnumerable<ItemInventory> GetInventories()
+        {
+            var items = InventoryBL.GetInventory();
+
+            return items;
+        }
+
+        [HttpPost("AddItem")]
+        public bool AddItem(ItemInventory request)
+        {
+            return InventoryBL.AddItem(request.ItemName, request.ItemQuantity, (int)request.ItemPrice);
+
+        }
+
+        [HttpDelete("DeleteItem")]
+        public bool RemoveItem(string itemName)
+        {
+            return InventoryBL.RemoveItem(itemName);
+
+        }
+
+        [HttpPatch("UpdateQuantity")]
+        public bool UpdateQuantity(string itemName, int itemQuantity)
+        {
+            var result = InventoryBL.UpdateQuantity(itemName, itemQuantity);
+            return result;
+        }
+
+        [HttpPatch("UpdatePrice")]
+        public bool UpdatePrice(string itemName, int itemPrice)
+        {
+            var result = InventoryBL.UpdatePrice(itemName, itemPrice);
+            return result;
+        }
+    }
+}
